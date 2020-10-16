55a8aa98d7ccefce322c5ca49d800daced332628
Vadim Goryainov <v.goryainov@tum.de>
2020-10-16 12:18:32 +0200

    map rendering

--- client/static/map_view.js
+++ client/static/map_view.js
@@ -19,3 +19,24 @@ const vm = new Vue({
     el: '#vm',
     delimiters: ['[[', ']]']
 });
+
+
+// Where you want to render the map.
+var element = document.getElementById('map');
+
+// Create Leaflet map on map element.
+var map = L.map(element);
+
+// Add OSM tile leayer to the Leaflet map.
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Target's GPS coordinates.
+var target = L.latLng('47.50737', '19.04611');
+
+// Set map's center to target with zoom 14.
+map.setView(target, 14);
+
+// Place a marker on the same location.
+L.marker(target).addTo(map);
